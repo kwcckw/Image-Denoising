065247e68384f92394b62209fff94e8f42da8d27
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2023-01-02 14:45:53 +0800

    Update option.py

--- PNGAN/util/option.py
+++ PNGAN/util/option.py
@@ -132,7 +132,7 @@ parser.add_argument('--resume', type=int, default=0,
                     help='resume from specific checkpoint')
 parser.add_argument('--print_model', action='store_true',
                     help='print model')
-parser.add_argument('--save_models', type=bool, default=True,,
+parser.add_argument('--save_models', type=bool, default=True,
                     help='save all intermediate models')
 parser.add_argument('--print_every', type=int, default=100,
                     help='how many batches to wait before logging training status')
